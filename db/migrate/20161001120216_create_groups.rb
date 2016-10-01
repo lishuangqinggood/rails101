2f993d07268d60ed04b8d10054ba59c1d18a1494
Lishuangqing <shuangqing@TIANYUANdeMacBook-Pro.local>
2016-10-01 20:22:43 +0800

    d

--- db/migrate/20161001120216_create_groups.rb
+++ db/migrate/20161001120216_create_groups.rb
@@ -1,8 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.title :string
-      t.description :text
+      t.string :title
+      t.text :description
+
       t.timestamps
     end
   end
